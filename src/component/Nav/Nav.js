4678735d668d0547ee855a580fafdd13706ccb99
mj <hi.minjungkim@gmail.com>
2020-04-20 22:35:12 +0900

    add nav and appear navlist when hover

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,11 +1,75 @@
 import React, { Component } from "react";
+import NavDetail from "./NavDetail/NavDetail";
 import "./Nav.scss";
+import logo from "../../image/logo.png";
+import logoOn from "../../image/logo_on.png"
 
 class Nav extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            show: "none",
+            bgColor: "rgba(0,0,0,0.2)",
+            fontColor: "white",
+            logo: logo,
+            smallNavColor: "rgba(255,255,255,0.7)",
+        }
+    }
+
+    hoverOn = () => {
+        this.setState({ 
+            show: "block", 
+            bgColor: "white", 
+            fontColor: "black", 
+            logo: logoOn,
+            smallNavColor: "#969696",
+        })
+    }
+
+    hoverOff = () => {
+        this.setState({ 
+            show: "none", 
+            bgColor: "rgba(0,0,0,0.2)", 
+            fontColor: "white", 
+            logo: logo,
+            smallNavColor: "rgba(255,255,255,0.7)",
+        })
+    }
+
     render() {
         return (
-            <div className="Nav">
-                Nav
+            <div 
+                className="Nav" 
+                style={{backgroundColor: this.state.bgColor, color: this.state.fontColor}}
+            >
+                <div className="logo"><img src={this.state.logo} /></div>
+                <div className="navList">
+                    <ul 
+                        onMouseOver={this.hoverOn}
+                        onMouseLeave={this.hoverOff}
+                    >
+                        <li><span className="aboutUs">ABOUT US</span></li>
+                        <li><span className="menu">MENU</span></li>
+                        <li><span className="store">STORE</span></li>
+                        <li><span className="society">SOCIETY</span></li>
+                        <li><span className="new">WHAT'S NEW</span></li>
+                        <li><span className="eShop">E-SHOP</span></li>
+                    </ul>
+                </div>
+                <div 
+                    className="smallNavList" 
+                    style={{color: this.state.smallNavColor}}
+                >
+                    <ul>
+                        <li><span>Login</span></li>
+                        <li><span>Join</span></li>
+                        <li><span>Contact us</span></li>
+                        <li><span>Recruit</span></li>
+                        <li><span>FAQ</span></li>
+                    </ul>
+                </div>
+                <NavDetail show={this.state.show}/>
             </div>
         );
     }
---
+++ src/component/Nav/NavDetail/NavDetail.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./NavDetail.scss";
+
+class NavDetail extends Component {
+    render() {
+        return (
+            <div
+                className="NavDetail"
+                style={{ display: this.props.show }}
+            >
+                <div>
+                    <ul className="aboutList">
+                        <li>WBC 챔피언 폴 바셋</li>
+                        <li>CEO 인사말</li>
+                        <li>Brand</li>
+                        <li>Barista</li>
+                        <li>Social Contribution</li>
+                    </ul>
+
+                    <ul className="menuList">
+                        <li>NEW</li>
+                        <li>COFFEE</li>
+                        <li>BEVERAGE</li>
+                        <li>ICE-CREAM</li>
+                        <li>FOOD</li>
+                        <li>PRODUCT</li>
+                    </ul>
+                    <ul className="storeList">
+                        <li>매장찾기</li>
+                    </ul>
+                    <ul className="societyList">
+                        <li>Paul Bassett Society</li>
+                        <li>회원 등급 혜택</li>
+                        <li>모바일 App안내</li>
+                        <li>e-Stamp 안내</li>
+                        <li>e-Card 안내</li>
+                        <li>제휴혜택 안내</li>
+                    </ul>
+                    <ul className="newList">
+                        <li>Event / News</li>
+                        <li>Coffee Class</li>
+                        <li>Notice</li>
+                    </ul>
+                    <ul className="eShopList">
+                        <li style={{fontWeight: "bold"}}>E-SHOP 소개</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NavDetail;
