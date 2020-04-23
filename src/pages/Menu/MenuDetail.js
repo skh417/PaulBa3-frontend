c7777d784a5b36e14051c8d791cd36dd0ee10093
심기현 <kihyunsim@simgihyeon-ui-MacBookPro.local>
2020-04-23 19:15:36 +0900

    update merge

---
+++ src/pages/Menu/MenuDetail.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./ManuDetail.scss";
+
+class ManuDetail extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
