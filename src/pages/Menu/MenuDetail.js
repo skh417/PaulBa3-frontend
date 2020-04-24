610314ab29ecbe88082fd7e0cd04d01c9d3a2203
심기현 <kihyunsim@simgihyeon-ui-MacBookPro.local>
2020-04-24 20:02:53 +0900

    modify layout and add fetch function

--- src/pages/Menu/MenuDetail.js
+++ src/pages/Menu/MenuDetail.js
@@ -3,40 +3,94 @@ import "./MenuDetail.scss";
 
 class MenuDetail extends Component {
     state = {
-        one: [],
+        coffee: null,
     }
     componentDidMount() {
-        fetch('/data/test.json')
+        fetch("http://10.58.0.25:8000/menu/detail?product=Machiato")
             .then( data => data.json())
-            .then( data => this.setState({one: data}, 
-                () => {console.log(this.state.one)}))
+            .then( data => this.setState({coffee: data}))
     }
 
     render() {
+        const { coffee } = this.state;
+        console.log(coffee)
+        if (!coffee) return (<></>);
+
         return(
-            <div className="menuDetail">
-                <div className="menuTitle">
-                    <dl>
-                        <dt>
-                            <span>Machiato</span>
-                            마키아토
-                        </dt>
-                        <dd>
-                            에스프레소의 소량의 우유 거품을 더해 부드러우면서 진한 커피의 조화로운 맛을 느낄 수 있습죠.
-                        </dd>
-                    </dl>
+            <div className="menuDetailBG">
+                <div className="menuDetail">
+                    <div className="menuTitle">
+                        <dl>
+                            <dt>
+                                <span>{coffee.menu.name_eng}</span>
+                                {coffee.menu.name_kor}
+                            </dt>
+                            <dd>
+                                {coffee.menu.description}
+                            </dd>
+                        </dl>
+                    </div>
                     <div className="menuImgContainer"></div>
                     <ul className="infomation">
                         <li>
                             <span>구분</span>
+                            {coffee.info.sort}
                         </li>
                         <li>
                             <span>알레르기 유발물질</span>
+                            {coffee.info.allergy}
                         </li>
                         <li>
                             <span>제공사이즈</span>
+                            {coffee.info.sizes}
                         </li>
                     </ul>
+                    <div className="nutriContainer">
+                        <div className="nutriTitle">    
+                            <span>
+                                영양 정보
+                                <span>1회 제공량 기준</span>
+                            </span>
+                            <select>
+                                <option>Original</option>
+                            </select>
+                        </div>
+                        <div className="nutriContents">
+                            <div>
+                                <span className="sizeML">
+                                    제공량(ml)
+                                    <span>70</span>
+                                </span>
+                                <ul className="nutriDetails">
+                                    <li>
+                                        <span className="tit">열량(kcal)</span>
+                                        <span className="nums">45</span>
+                                    </li>
+                                    <li>
+                                        <span className="tit">단백질(g)</span>
+                                        <span className="nums">2</span>
+                                    </li>
+                                    <li>
+                                        <span className="tit">당류(g)</span>
+                                        <span className="nums">2</span>
+                                    </li>
+                                    <li>
+                                        <span className="tit">나트륨(mg)</span>
+                                        <span className="nums">20</span>
+                                    </li>
+                                    <li>
+                                        <span className="tit">포화지방(g)</span>
+                                        <span className="nums">2.2</span>
+                                    </li>
+                                    <li>
+                                        <span className="tit">카페인(mg)</span>
+                                        <span className="nums">81</span>
+                                    </li>
+                                </ul>
+                                <p>* 우유가 추가된 음료의 영양정보는 오리지널 우유 기준입니다.</p>
+                            </div>
+                        </div>    
+                    </div>
                 </div>
             </div>
         )
