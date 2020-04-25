890d8c0e20448fd32ef073daaae70bbcd1ef5204
mj <hi.minjungkim@gmail.com>
2020-04-25 10:32:12 +0900

    done with one-view scroll event

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,45 +8,39 @@ class Main extends Component {
         super(props);
         this.oneRef = React.createRef();
         this.twoRef = React.createRef();
-        this.state = {
-            nowTop: 0,
-        }
-    }
-
-    componentDidMount() {
-        // window.addEventListener('scroll', this.moveScroll);
     }
 
     moveScroll = (e) => {
-        // console.log('moveScroll 함수 작동')
-        console.log('oneRef.offsetTop', this.oneRef.offsetTop)
-        console.log('twoRef.offsetTop', this.twoRef.offsetTop)
-        
         if (e.nativeEvent.wheelDelta > 0) {
             // oneReft로 이동
             console.log('oneRef로')
-            // window.scroll(0, this.oneRef.offsetTop)
-            this.setState({nowTop: this.oneRef.offsetTop}, () => window.scrollTo(0, this.state.nowTop))
-        
+            window.scroll({
+                top: this.oneRef.offsetTop, 
+                left: 0,
+                behavior: 'smooth',
+            })
         } else {
             // twoRef로 이동
             console.log('twoRef로')
-            // window.scroll(0, this.twoRef.offsetTop)
-            this.setState({nowTop: this.twoRef.offsetTop}, () => window.scrollTo(0, this.state.nowTop))
+            window.scroll({
+                top: this.twoRef.offsetTop, 
+                left: 0,
+                behavior: 'smooth',
+            })
         }
-        
+
     }
 
     render() {
 
         return (
             <div className="Main" onWheel={this.moveScroll}>
-                <div 
-                    ref={(ref) => { this.oneRef = ref }} 
+                <div
+                    ref={(ref) => { this.oneRef = ref }}
                 >
                     <MainOne />
                 </div>
-                <div 
+                <div
                     ref={(ref) => { this.twoRef = ref }}
                 >
                     <MainTwo />
