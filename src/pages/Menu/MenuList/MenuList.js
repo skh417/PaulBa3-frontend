d66e06355dcae1e9460bcbc2700524955fc4de50
mj <hi.minjungkim@gmail.com>
2020-04-29 13:44:15 +0900

    make menuAll.js and export, put key in map function

--- src/pages/Menu/MenuList/MenuList.js
+++ src/pages/Menu/MenuList/MenuList.js
@@ -3,69 +3,8 @@ import { Link, withRouter } from "react-router-dom";
 import Nav from "../../../component/Nav/Nav";
 import MenuCard from "./MenuCard/MenuCard";
 import MenuFooter from "../MenuFooter/MenuFooter";
+import { menuAll } from "./menuAll";
 import "./MenuList.scss";
-
-const menuAll = {
-  coffee: {
-    title: "COFFEE",
-    description: "한잔의 완벽한 커피",
-    categories: [
-      "전체",
-      "Coffee",
-      "Lattee",
-      "Espresso",
-      "Cold Brew",
-      "Single Origin",
-    ],
-    banner:
-      "https://www.baristapaulbassett.co.kr/images/menu/subVisual_coffee.jpg",
-  },
-  beverage: {
-    title: "BEVERAGE",
-    description: "폴 바셋의 특별한 레시피로 완성된 non coffee 음료",
-    categories: [
-      "전체",
-      "Frappe",
-      "Tea Lattee",
-      "Ade Juice",
-      "Tea",
-      "Chocolate",
-      "Yogurt",
-    ],
-    banner:
-      "https://www.baristapaulbassett.co.kr/images/menu/subVisual_beverage.jpg",
-  },
-  "ice-cream": {
-    title: "ICE-CREAM",
-    description:
-      "상하목장 유기농 우유로 만든 깊고 풍부한 유풍미의 프리미엄 소프트 아이스크림",
-    categories: ["전체", "Milk Ice-Cream", "Season Ice-Cream"],
-    banner:
-      "https://www.baristapaulbassett.co.kr/images/menu/subVisual_iceCream.jpg",
-  },
-  food: {
-    title: "FOOD",
-    description:
-      "폴 바셋 프리미엄 푸드로 때로는 달콤함을, 때로는 든든함을 느껴보세요.",
-    categories: ["전체", "Bakery", "Morning foods", "Nata", "Cake Dessert"],
-    banner:
-      "https://www.baristapaulbassett.co.kr/images/menu/subVisual_food.jpg",
-  },
-  product: {
-    title: "PRODUCT",
-    description: "폴 바셋의 클래식 MD 상품들을 만나보세요",
-    categories: [
-      "전체",
-      "Barista Pouch",
-      "Whole bean",
-      "Tumbler",
-      "Mug",
-      "ETC",
-    ],
-    banner:
-      "https://www.baristapaulbassett.co.kr/images/menu/subVisual_product.jpg",
-  },
-};
 
 class MenuList extends Component {
   constructor(props) {
@@ -112,7 +51,7 @@ class MenuList extends Component {
             <div className='all'>
               {menuAll[category].categories.map((cat, index) => {
                 return (
-                  <Link to={`/menu/${category}/${index}`}>
+                  <Link to={`/menu/${category}/${index}`} key={index}>
                     <div id={index}>{cat}</div>
                   </Link>
                 );
@@ -124,7 +63,10 @@ class MenuList extends Component {
               {theOne.products === undefined
                 ? products.map((item, index) => {
                     return (
-                      <Link to={`/menu/detail?product=${item.name_eng}`}>
+                      <Link
+                        to={`/menu/detail?product=${item.name_eng}`}
+                        key={index}
+                      >
                         <MenuCard
                           key={index}
                           korName={item.name_kor}
@@ -139,7 +81,10 @@ class MenuList extends Component {
                   })
                 : theOne.products.map((item, index) => {
                     return (
-                      <Link to={`/menu/detail?product=${item.name_eng}`}>
+                      <Link
+                        to={`/menu/detail?product=${item.name_eng}`}
+                        key={index}
+                      >
                         <MenuCard
                           key={index}
                           korName={item.name_kor}
