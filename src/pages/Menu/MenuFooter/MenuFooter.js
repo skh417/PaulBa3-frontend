8d60a1638c40dcd26dd23dd12590e4b32009f9fd
심기현 <kihyunsim@simgihyeon-ui-MacBookPro.local>
2020-05-01 11:59:49 +0900

    code refactoring

--- src/pages/Menu/MenuFooter/MenuFooter.js
+++ src/pages/Menu/MenuFooter/MenuFooter.js
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom';
 import "./MenuFooter.scss";
 
 class MenuFooter extends Component {
-  state = {
-    changeClass: "",
-  };
+  constructor(){
+    super();
+    this.state = {
+      changeClass: "",
+    };
+  }
+  
+  clickToMove = () => {
+    window.scrollTo(0,0);
+  }
 
   beverageImgOn = () => {
     this.setState({
@@ -55,18 +62,24 @@ class MenuFooter extends Component {
       <div className='MenuFooter'>
         <ul className={changeClass}>
           <li>
-            <Link to="/menu/coffee/0">COFFEE</Link>
+            <Link to="/menu/coffee/0" 
+              onClick={this.clickToMove}>COFFEE
+            </Link>
           </li>
           <li>
             <Link to="/menu/beverage/0"
               onMouseEnter={this.beverageImgOn}
               onMouseLeave={this.beverageImgOff}
+              onClick={this.clickToMove}
             >
               BEVERAGE
             </Link>
           </li>
           <li>
-            <Link to="/menu/food/0" onMouseEnter={this.foodImgOn} onMouseLeave={this.foodImgOff}>
+            <Link to="/menu/food/0" 
+              onMouseEnter={this.foodImgOn} 
+              onMouseLeave={this.foodImgOff} 
+              onClick={this.clickToMove}>
               FOOD
             </Link>
           </li>
@@ -74,6 +87,7 @@ class MenuFooter extends Component {
             <Link to="/menu/ice-cream/0"
               onMouseEnter={this.icecreamImgOn}
               onMouseLeave={this.icecreamImgOff}
+              onClick={this.clickToMove}
             >
               ICE-CREAM
             </Link>
@@ -82,6 +96,7 @@ class MenuFooter extends Component {
             <Link to="/menu/product/0"
               onMouseEnter={this.productImgOn}
               onMouseLeave={this.productImgOff}
+              onClick={this.clickToMove}
             >
               PRODUCT
             </Link>
